28d59accd1ee2fa15d7bc710a9eac904baa8c93c
jirawittn <jirawitth@kkumail.com>
2022-07-22 22:51:04 +0700

    master

---
+++ README.md
@@ -0,0 +1,1 @@
+My Personal web site is at https://jirawittn.github.io/ and its index.html is at https://github.com/Jirawittn/jirawittn.github.io
---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+// var iceCream = 'Chocolate';
+// if (iceCream == 'Chocolate'){
+//     alert('Yay, I love chocolate ice cream!');
+// }else {
+//     alert('Awwww, but chocolate is my favorite...');
+// }
